90f9a35835c94a50e004777979e20de5fb8219dd
Ranjan kumar <jenaranjankumar788@gmail.com>
2025-09-17 20:52:05 +0530

    Update app.py

--- greeksprofile/greeksporofile/app.py
+++ greeksprofile/greeksporofile/app.py
@@ -19,7 +19,7 @@ mysql = MySQL(app)
 
 
 # ------------------ LOGIN ------------------
-@app.route('/')
+# @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
@@ -150,3 +150,4 @@ def update():
 # ------------------ MAIN ------------------
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
+
